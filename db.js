9698c3bcb11506d41def92d3a92fc557234e2dc8
seabelis <seabelis74@gmail.com>
2019-11-04 14:04:07 +0100

    database connected

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:password@localhost:5434/postgres";
+const db = new Sequelize(databaseUrl)
+
+  .sync()
+  .then(() => console.log("Database connected"))
+  .catch(console.error);
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 4000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Init
+const express = require("express");
+const app = express();
+const port = 4000;
+// Models & DB
+const db = require("./db");
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
