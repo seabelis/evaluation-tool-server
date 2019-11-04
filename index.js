ba5b101b2f09e68a538f0a290467fb23800f6eec
seabelis <seabelis74@gmail.com>
2019-11-04 18:19:21 +0100

    users model created

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+
 // Models & DB
 const db = require("./db");
+const User = require("./user/model");
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
