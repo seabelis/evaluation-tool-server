9df9e4f16354c2b81883c8ab5181165d05202262
seabelis <seabelis74@gmail.com>
2019-11-05 10:03:06 +0100

    Houston, we have a user.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-// Init
 const express = require("express");
 const app = express();
 const port = 4000;
-
-// Models & DB
 const db = require("./db");
+
+// Body-Parser
+const bodyParser = require('body-parser')
+
+// Cors
+const cors = require("cors");
+const corsMiddleware = cors();
+
+// Models
 const User = require("./user/model");
-const Batch = require("./batch/model")
-const Student = require("./student/model")
-const Evaluation = require("./evaluation/model")
+const Batch = require("./batch/model");
+const Student = require("./student/model");
+const Evaluation = require("./evaluation/model");
 
+// Routers
+const userRouter = require("./user/router");
 
+app.use(corsMiddleware);
+app.use(bodyParser.json())
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(userRouter);
